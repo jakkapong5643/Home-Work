f409ea1538e6dfd421bc0f6d28d62944ea4fc980
jakkapong5643 <69631237+jakkapong5643@users.noreply.github.com>
2022-09-05 18:05:14 +0700

    Update HW 04.py

--- HW 04.py
+++ HW 04.py
@@ -1,18 +1,29 @@
-def fast_charge(self, coupler_inlet, max_capacity, current_percent, target_percent):
+class Car:
+    def fast_Change(self, coupler_inlet, max_capacity, current_percent, target_percent):
+        self.coupler_inlet = coupler_inlet
+        self.max_capacity = max_capacity
+        self.current_percent = current_percent
+        self.target_percent = target_percent
 
-    dict = {
-        "Tesla" : A == 135,
-        "CSS Type 2" : A == 175,
-        "CSS Type 1" : A ==150,
-        "GB/T" : A == 120,
-        "CHAdeMo" : A == 400
-    }
+        dict = {
+            self.coupler_inlet == "Tesla": 135,
+            self.coupler_inlet == "CSS Type 2" : 175,
+            self.coupler_inlet == "CSS Type 1" : 150,
+            self.coupler_inlet == "GB/T" : 120,
+            self.coupler_inlet == "CHAdeMo" : 400
+        }
     ## method
-    def Method():
-        percent_diff = round((target_percent - current_percent))
-        target_kw = round((max_capacity * (percent_diff /100)))
-        finish_time = round(((target_kw / A) * 60))
-        return finish_time
-a = fast_Change("CSS Type 2",65.7,10,60)
-print(a)
+    def percent_diff(self):
+        self.percent_diff = self.target_percent - self.current_percent
     
+    def target_kw(self):
+        Car.percent_diff(self)
+        self.target_kw = self.max_capacity * (self.percent_diff/100)
+    
+    def finish(self):
+        Car.target_kw(self)
+        finish_time = (self.target_kw/self.coupler_inlet)*60
+        return finish_time
+
+A = fast_Change("CHAdeMo",65.7,10,60)
+print(A)
